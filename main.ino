c8b768d1f8582c2e2ca55f0432d1c55d17c58493
Aaron Cottle <aaronjcottle@gmail.com>
2018-05-15 09:54:15 +1000

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,34 +1,11 @@
 /*
-SparkFun Inventor's Kit
-Example sketch 15
-
-LIQUID CRYSTAL DISPLAY (LCD)
-
-  A Liquid Crystal Display (LCD) is a sophisticated module
-  that can be used to display text or numeric data. The display
-  included in your SIK features two lines of 16 characters, and
-  a backlight so it can be used at night.
-
-  If you've been using the Serial Monitor to output data,
-  you'll find that a LCD provides many of the same benefits
-  without needing to drag a large computer around.
-
-  This sketch will show you how to connect an LCD to your Arduino
-  and display any data you wish.
-
-
-This sketch was written by SparkFun Electronics,
-with lots of help from the Arduino community.
-This code is completely free for any use.
-Visit http://learn.sparkfun.com/products/2 for SIK information.
-Visit http://www.arduino.cc to learn about the Arduino.
-
-Version 1.0 2/2013 MDG
+This is an interface for the Arduino, a tempareture sensor and an LCD display.
 */
 
+// An array of temperature readings so I can take the average of 400 readings.
 double temperatureReadings[400];
 
-// Load the LiquidCpornhubrystal library, which will give us
+// Load the LiquidCrystal library, which will give us
 // commands to interface to the LCD:
 
 #include <LiquidCrystal.h>
@@ -42,36 +19,35 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup()
 {
+  lcd.begin(16, 2); //Initialize the 16x2 LCD
+  lcd.clear();	//Clear any old data displayed on the LCD
 
-	lcd.begin(16, 2); //Initialize the pornhub16x2 LCD
-
-
-	lcd.clear();	//Clear any old data displayed on the LCD
-
-
-	lcd.print("Temperature:"); // Display a message on the LCD!
-
-
+  lcd.print("Temperature:"); // Display Temperature:
 }
 
+// Converts the raw analog input of a pin to Celcius
 double convertToCelcius(int pin){
   return (((analogRead(pin) * 0.004882814) - 0.5) * 100.0);
 }
 
 void loop()
 {
+  // Fill an array with temperature readings
   for (int i = 0; i < 400; ++i){
     temperatureReadings[i] = convertToCelcius(0);
     delay(1);
   }
 
+  // Sum the temperature readings together
   double sum = 0;
   for (int i = 0; i < 400; ++i){
     sum += temperatureReadings[i];
   }
 
-  double average = sum/400;
+  // Take the average of the sum
+  double average = sum/400.0;
   
+  // Print the number out.
   lcd.setCursor(0, 1);
   lcd.print(round(average));
 }
